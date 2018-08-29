8c7c4fb656770c81e3cc076ee3a8ad20f883a233
Gs Mahardika <gs.mahardika@gmail.com>
2018-08-29 16:13:53 +0800

    Example on using async/await

--- components/test.js
+++ components/test.js
@@ -31,8 +31,36 @@ const processData = (res, data, dataExtended) => {
   })
 }
 
+const getAnotherData = async (res) => {
+  let query = new Promise((resolve, reject) => {
+    mysql.query('SELECT * FROM test', (err, rows) => {
+      if (err) {
+        throw err
+      }
+
+      console.log(2)
+
+      resolve(rows)
+    })
+  })
+
+  console.log(1)
+  let data = await query
+  console.log(3)
+
+  res.json({
+    status: true,
+    message: 'Hello world from /test/data!',
+    data: data
+  })
+}
+
 router.get('/user', (req, res, next) => {
   getData(res)
+})
+
+router.get('/data', (req, res, next) => {
+  getAnotherData(res)
 })
 
 module.exports = router
